59e82b92e5943d63d391cd0334321080ed4e3f98
harshitj99 <harshitjain137@gmail.com>
2022-04-20 17:32:07 +0530

    Changed Functionality

--- src/demo.java
+++ src/demo.java
@@ -3,7 +3,7 @@ class Demo{
 public static void main(String [] args){
 Scanner sc = new Scanner(System.in);
 int n = sc.nextInt();
-for(int i=1;i<n;i++){
+for(int i=1;i<=n;i++){
 System.out.println(i);
 }
 }
